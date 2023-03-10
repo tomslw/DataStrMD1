3ef59b85352bd2851546971c8a9967d7a4140582
Toms Strals <stralstomslw@gmail.com>
2023-03-10 14:48:28 +0200

    Added additional tests for MyStack

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -12,18 +12,51 @@ public class MainService {
 		
 		// Numeric type
 		System.out.println("Numeric type Stack:");
+		System.out.println("Executing .push() functions");
 		intStack.push(1);
 		intStack.push(2);
 		intStack.push(3);
+		System.out.println("Executing .print() function");
+		intStack.print();
+		System.out.println("Executing .isEmpty() function");
+		System.out.println(intStack.isEmpty());
+		System.out.println("Executing .getLength() function");
+		System.out.println(intStack.getLength());
+		System.out.println("Executing .pop() function, then print()");
+		intStack.pop();
+		intStack.print();
+		System.out.println("Executing .top() function");
+		System.out.println(intStack.top());
+		System.out.println("Executing .empty() function, then getLength(), then isEmpty(), then print()");
+		intStack.empty();
+		System.out.println(intStack.getLength());
+		System.out.println(intStack.isEmpty());
 		intStack.print();
 		System.out.println();
 
 		// Students type
 		System.out.println("Student type Stack:");
+		System.out.println("Executing .push() functions");
 		studStack.push(new Student("Janis", "Berzins", Faculty.EPF));
 		studStack.push(new Student("Kristaps", "Miezitis", Faculty.ITF));
 		studStack.push(new Student("Maigonis", "Baigais", Faculty.TSF));
+		System.out.println("Executing .print() function");
 		studStack.print();
+		System.out.println("Executing .isEmpty() function");
+		System.out.println(studStack.isEmpty());
+		System.out.println("Executing .getLength() function");
+		System.out.println(studStack.getLength());
+		System.out.println("Executing .pop() function, then print()");
+		studStack.pop();
+		studStack.print();
+		System.out.println("Executing .top() function");
+		System.out.println(studStack.top());
+		System.out.println("Executing .empty() function, then getLength(), then isEmpty(), then print()");
+		studStack.empty();
+		System.out.println(studStack.getLength());
+		System.out.println(studStack.isEmpty());
+		studStack.print();
+		System.out.println();
 	}
 
 }
